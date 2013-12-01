a7b6ade8295581fb12688b352a91c10ba419672a
lemniscata <lemniscata.lmn@gmail.com>
2013-12-01 15:36:43 +0100

    modified skeleton.cpp and added a new problem

--- skeleton.cpp
+++ skeleton.cpp
@@ -2,7 +2,7 @@
    \author Pablo Cabeza García
 
    \problem [ProblemNumber]-[ProblemName]
-   \time [Time-restriction
+   \time [Time-restriction]
 
    \brief [Problem-Description]
 
@@ -13,8 +13,8 @@
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
-
 #include <cstring>
+
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -46,33 +46,58 @@ using namespace std;
 
 
 ///
-///	TYPE DEFINITIONS
+///	TYPE DEFINITIONS AND OTHERS
 /// 
 
+typedef bool B;
+typedef char C;
 typedef int I;
+typedef unsigned U;
 typedef long L;
+typedef long unsigned lU;
 typedef long long LL;
+typedef long long unsigned LU;
 typedef double D;
 typedef long double LD;
 
-#define v(t) vector((t))
-#define p(a,b) pair<(a),(b)>
-#define m(a,b) map<(a),(b)>
-#define q(a) queue<(a)>
-#define s(a) s<(a)>
-#define IT iterator
+#define V(t) vector((t))
+#define P(a,b) pair<(a),(b)>
+#define M(a,b) map<(a),(b)>
+#define Q(a) queue<(a)>
+#define S(a) s<(a)>
+#define T iterator
+
+#define MC(a,b) memset((a),0,(b))
+#define MS(a,b) memset((a),-1,(b))
+#define SZ(a) sizeof((a))
+
+
+///
+///	SEQ-Definitions
+/// 
+
+// Equivalent to: for(I i=0...) for(I j=0...){  }
+// To do something after every i loop use if(j==k-1){ ... }
+#define BISEQ(i,j,k) for(I (i)=0,(j)=0;(i)<(k);(i)+=0==((j)=++(j)%(k)))
+
+// does for loop in range [l,k) (if l>k it does the reverse)
+#define SEQ(i,l,k) for(I (i)=(l),_i_=(k)-(l)>=0?1:-1;(i)!=(k);(i)+=_i_)
 
 
 ///
 ///	FAST-IO
 /// 
 
+#define gc getchar
+#define pc putchar
+#define pf printf
+
 template <typename type>
 inline type rdnumber(){
     type out; int c,sign;
-    while((c=getc(stdin))==' ' || c=='\n');
+    while((c=gc())==' ' || c=='\n');
     out=(c=='-')?(sign=-1,0):(sign=1,c-'0');
-    while((c=getc(stdin))>='0' && c<='9') 
+    while((c=gc())>='0' && c<='9') 
 	out=(out<<3)+(out<<1)+c-'0';
     ungetc(c,stdin);
     return out*sign;
@@ -85,9 +110,9 @@ inline type rdnumber(){
 template <typename type>
 inline type rdunumber(){
     type out; int c;
-    while((c=getc(stdin))==' ' || c=='\n');
+    while((c=gc())==' ' || c=='\n');
     out=(c>='0' && c<='9')?c-'0':0;
-    while((c=getc(stdin))>='0' && c<='9') 
+    while((c=gc())>='0' && c<='9') 
 	out=(out<<3)+(out<<1)+c-'0';
     ungetc(c,stdin);
     return out;
